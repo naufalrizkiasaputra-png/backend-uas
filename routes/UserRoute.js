f2a2018fa2d2ce09196911c5165181a8ef6f7d6f
naufalrizkiasaputra-png <naufal.rizkia.saputra@gmail.com>
2026-01-17 00:29:17 +0700

    nambah UserRoutejs login

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -1,12 +1,11 @@
 import express from "express";
-import {
-    getUsers,
-    createUser
-} from "../controllers/UserController.js";
+import { getUsers, createUser } from "../controllers/UserController.js";
+import { Login } from "../controllers/Auth.js"; // <-- Import Login
 
 const router = express.Router();
 
-router.get('/users', getUsers);  // Buat liat data user (Method GET)
-router.post('/users', createUser); // Buat register user baru (Method POST)
+router.get('/users', getUsers);
+router.post('/users', createUser);
+router.post('/login', Login); // <-- Tambah Route Login
 
 export default router;
