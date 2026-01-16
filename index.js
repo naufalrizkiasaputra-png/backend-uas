7b54d1898ece00c6f06f44f22b8ff52330e5180f
naufalrizkiasaputra-png <naufal.rizkia.saputra@gmail.com>
2026-01-17 00:29:51 +0700

    ubah indexjs2

--- index.js
+++ index.js
@@ -2,13 +2,11 @@ import express from "express";
 import cors from "cors";
 import db from "./config/database.js";
 import UserRoute from "./routes/UserRoute.js";
+import JobRoute from "./routes/JobRoute.js"; // <-- TAMBAHAN 1
 
-// --- BAGIAN INI YANG BIKIN TABEL MUNCUL ---
-// Kamu wajib import file modelnya di sini walau tidak dipakai variable-nya
 import "./models/UserModel.js";
 import "./models/JobModel.js";
 import "./models/ApplicationModel.js";
-// ------------------------------------------
 
 const app = express();
 const port = 5000;
@@ -17,10 +15,7 @@ const port = 5000;
     try {
         await db.authenticate();
         console.log('Database Connected...');
-        
-        // Nyalakan ini sekali, save, tunggu terminal jalan, cek phpMyAdmin
-        await db.sync(); 
-        
+        // await db.sync(); // Udah dimatiin karena tabel udah ada
     } catch (error) {
         console.error('Connection Error:', error);
     }
@@ -28,6 +23,8 @@ const port = 5000;
 
 app.use(cors());
 app.use(express.json());
+
 app.use(UserRoute);
+app.use(JobRoute); // <-- TAMBAHAN 2
 
 app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
