42fc899f5be6286924a968b276eca0b293b0d0b1
naufalrizkiasaputra-png <naufal.rizkia.saputra@gmail.com>
2026-01-17 00:01:06 +0700

    ubah index1

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
 import express from "express";
-import cors from "cors"; // Pastikan sudah install: npm install cors
+import cors from "cors";
 import db from "./config/database.js";
 import UserRoute from "./routes/UserRoute.js";
-// import Users from "./models/UserModel.js"; // Uncomment ini kalau mau generate tabel otomatis pertama kali
+
+// --- BAGIAN INI YANG BIKIN TABEL MUNCUL ---
+// Kamu wajib import file modelnya di sini walau tidak dipakai variable-nya
+import "./models/UserModel.js";
+import "./models/JobModel.js";
+import "./models/ApplicationModel.js";
+// ------------------------------------------
 
 const app = express();
 const port = 5000;
 
-// Tes koneksi database & Sync
 (async () => {
     try {
         await db.authenticate();
         console.log('Database Connected...');
-        // await Users.sync(); // Uncomment ini sekali aja buat bikin tabel, terus comment lagi
+        
+        // Nyalakan ini sekali, save, tunggu terminal jalan, cek phpMyAdmin
+        await db.sync(); 
+        
     } catch (error) {
         console.error('Connection Error:', error);
     }
 })();
 
 app.use(cors());
-app.use(express.json()); // Biar bisa terima data JSON
-
-// Gunakan Route
+app.use(express.json());
 app.use(UserRoute);
 
 app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
