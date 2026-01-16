737941b5f2d7991b14f58d752cda863883124320
naufalrizkiasaputra-png <naufal.rizkia.saputra@gmail.com>
2026-01-17 00:34:22 +0700

    nambah UserRoutejs register

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { getUsers, createUser } from "../controllers/UserController.js";
-import { Login } from "../controllers/Auth.js"; // <-- Import Login
+import { Login } from "../controllers/Auth.js";
 
 const router = express.Router();
 
 router.get('/users', getUsers);
-router.post('/users', createUser);
-router.post('/login', Login); // <-- Tambah Route Login
+router.post('/users', createUser); // Ini sebenernya register juga
+router.post('/register', createUser); // <-- KITA TAMBAH INI BIAR JELAS
+router.post('/login', Login);
 
 export default router;
