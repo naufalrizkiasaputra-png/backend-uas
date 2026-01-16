56c9f04b8bbdd3b6dfdf7a127e5dcb8db2d1a50e
naufalrizkiasaputra-png <naufal.rizkia.saputra@gmail.com>
2026-01-16 23:10:04 +0700

    ganti UserModel

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,16 +1,46 @@
 import { Sequelize } from "sequelize";
 import db from "../config/database.js";
-const {DataTypes} = Sequelize;
 
-const User = db.define('users',{
-    name:DataTypes.STRING,
-    email:DataTypes.STRING,
-    gender:DataTypes.STRING
-},{
-    freezeTableName:true
+const { DataTypes } = Sequelize;
+
+const Users = db.define('users', {
+    uuid: {
+        type: DataTypes.STRING,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        validate: { notEmpty: true }
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: { notEmpty: true, len: [3, 100] }
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: { notEmpty: true, isEmail: true }
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: { notEmpty: true }
+    },
+    role: {
+        type: DataTypes.STRING, 
+        defaultValue: "seeker", // Bisa: 'seeker', 'company', 'admin'
+        allowNull: false
+    },
+    // Khusus Company: Nama Perusahaan & Lokasi
+    company_name: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    location: {
+        type: DataTypes.STRING,
+        allowNull: true
+    }
+}, {
+    freezeTableName: true
 });
-export default User;
 
-(async()=>{
-    await db.sync();
-})();                                               
+export default Users;
